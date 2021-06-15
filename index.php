81ee0be9a82299245070127d83e4060eae80d86b
aalejog09 <52252251+aalejog09@users.noreply.github.com>
2021-06-15 17:14:40 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,217 @@
+<?php 
+
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="description" content="Somos el Aliado que tu necesitas, Ofrecemos Servicios y Comercialización de Puntos de Venta inalambricos." />
+	<meta name="keywords" content="PagoNet, Puntos de Venta, mPOS, POS" />
+	<meta name="msapplication-TileColor" content="#ffffff" />
+	<meta name="msapplication-TileImage" content="images/ms-icon-144x144.png" />
+	<meta name="theme-color" content="#ffffff" />
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="theme-color" content="#ffffff">
+	<meta name="author" content="Servicios PagoNet C.A.">
+	<meta property="og:title" content="PagoNet | Venta Puntos de Venta" />
+	<meta property="og:image" content="http://pagonet.info/images/socialmedia/pagonet-home.jpg" />
+	<meta property="og:type" content="website" />
+	<meta property="og:locale" content="es_ES" />
+	<meta property="og:site_name" content="PagoNet | Venta Puntos de Venta" />
+	<meta property="og:description" content="Somos el Aliado que tu necesitas, Ofrecemos Servicios y Comercialización de Puntos de Venta inalambricos." />
+	<meta property="twitter:description" content="Somos el Aliado que tu necesitas, Ofrecemos Servicios y Comercialización de Puntos de Venta inalambricos." />
+	<meta property="twitter:image" content="http://pagonet.info/images/socialmedia/pagonet-home.jpg" />
+	<meta property="twitter:title" content="PagoNet | Venta Puntos de Venta" />
+	<meta name="google-site-verification" content="zbf_SHwIcoqGpUkhRu777uTgIZm3JLYFIGFb6lOPi1Y" />
+	<link rel="canonical" href="http://pagonet.info/" />
+	<link rel="shortlink" href="http:/pagonet.info/" />
+	<link rel="shortcut icon" href="images/favicon-32.png" />
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" sizes="57x57" href="images/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="images/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="images/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="images/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="images/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="images/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="images/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+	<link rel="manifest" href="images/manifest.json">
+	<meta name="google-site-verification" content="WxzEqO5SHx_CZOdDFuo7UAkutWhvtbM6Nh8gJsmgtPk" />
+	<title>PagoNet | Venta Puntos de Venta</title>
+	<link rel="stylesheet" href="style.css">
+	<!-- Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-132851822-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-132851822-1');
+	</script>
+	</head>
+
+	<body>
+	<header>
+	  <div class="home img"></div>
+	  <div class="home shadow">
+	    <section>
+	      <div class="header">
+	        <embed src="resources/svg/logo1.svg" type="">
+	        <ul class="ul display">
+	          <li> <a href="" class="active"> Inicio </a> </li>
+	          <li> <a href="#nosotros"> Nosotros </a> </li>
+	          <li> <a href=""> Contacto </a> </li>
+	          <li class="information"> <a href="#informacion"> Información </a> </li>
+	        </ul>
+	      </div>
+	      <div class="homePrinc">
+	        <div>
+	          <div>
+	            <div class="slogan">¡LA SOLUCIÓN</div>
+	            <div class="slogan s2">PARA TU NEGOCIO!</div>
+	          </div>
+	          <p>Somos la conexión perfecta entre los clientes y tus metas como empresario, ¿quieres más información? </p>
+	          <span>Escríbenos a:</span> <strong>institucional@pagonet.info</strong> </div>
+	        <img src="resources/img/pointOfSales.png" alt=""> </div>
+	    </section>
+	  </div>
+	  <img class="menu" src="resources/img/menu-button.png" alt=""> <img class="point375" src="resources/img/pointOfSales.png" alt=""> </header>
+	<main>
+	  <article id="nosotros">
+	    <section>
+	      <embed class="logo2" src="resources/svg/logo2.svg" type="">
+	      <p class="p1">Somos el aliado que necesitas para seguir expandiendo tu negocio y adaptarte a las necesidades del mercado venezolano.</p>
+	      <embed src="resources/svg/icon1.svg" type="">
+	      <div>
+	        <h3 class="h3Light">Negocios en crecimiento</h3>
+	        <h3>CON GRANDES SUEÑOS</h3>
+	      </div>
+	      <p class="p2">La opción ideal para empresarios que buscan expandir sus ventas y mantenerse competitivos con alternativas de pago electrónico.</p>
+	      <p class="p3">Ideal para quienes necesitan ofrecer soluciones de pago eficientes y rápidas que se adaptan al estilo de vida de profesionales independientes, profesores particulares, emprendedores y comerciantes en general que desean ofrecer siempre lo mejor a sus clientes.</p>
+	      <div class="oneLine"><strong> Regístrate siguiendo tres sencillos pasos: </strong></div>
+	    </section>
+	  </article>
+	  <article>
+	    <section class="passes">
+	      <div>
+	        <embed src="resources/svg/iconPass1.svg" type="">
+	        <div class="oneLine">Obtén tu Nº de afiliación</div>
+	        <p>Completa el registro correspondiente y tendrás la orden personalizada con un código que identifica al dispositivo asignado a tu afiliación.</p>
+	      </div>
+	      <div>
+	        <embed src="resources/svg/iconPass2.svg" type="">
+	        <div class="oneLine">Recibe tu dispositivo</div>
+	        <p>Cuenta con un sistema integrado de impresora térmica que es rápida y eficiente, con una batería recargable de alta capacidad.</p>
+	      </div>
+	      <div>
+	        <embed src="resources/svg/iconPass3.svg" type="">
+	        <div class="oneLine">Realiza tus ventas</div>
+	        <p>Empieza a hacer transacciones móviles 100% seguras e impulsa tu negocio con la confiabilidad que tus clientes necesitan.</p>
+	      </div>
+	    </section>
+	    <section>
+	      <div>
+	        <div class="oneLine"><strong> ¡Impulsa tu negocio, talento o servicio con nosotros! </strong></div>
+	        <p class="p4">Sin importar el tamaño de tu empresa, somos el aliado que necesitas para concretar todas tus ventas de manera confiable y exitosa, porque <strong>PagoNet</strong> te ofrece el punto de venta que se adapta a tus necesidades.</p>
+	      </div>
+	    </section>
+	  </article>
+	  <article>
+	    <section class="banner">
+	      <div> <img src="resources/img/pointOfSalesMini.png" alt="">
+	        <div>
+	          <h3>WisePad ™ 2 Plus</h3>
+	          <p>Ofrecemos a nuestros clientes un dispositivo versátil y de alto rendimiento, permitiendo integración de diversas aplicaciones y textos en diferentes lenguajes. Asimismo, contiene una batería recargable de alta capacidad que brinda a cada emprendedor la posibilidad de ser competitivos en el mercado cambiante que vivimos actualmente.</p>
+	        </div>
+	      </div>
+	    </section>
+	  </article>
+	  <article>
+	    <section>
+	      <div class="R1">
+	        <embed src="resources/svg/1svg.svg" type="">
+	        <p>¡Olvídate de complicaciones con el efectivo! En <strong>PagoNet</strong> nos encargamos de hacer de tus transacciones algo sistematizado y rápido, donde ofrecemos confiabilidad a todos nuestros clientes y establecimientos afiliados.</p>
+	      </div>
+	      <div class="R2">
+	        <embed src="resources/svg/2svg.svg" type="">
+	        <p>Nos adaptamos al estilo de vida de los emprendedores y grandes empresarios, brindándoles la oportunidad de registrarse y empezar a realizar sus pagos desde cualquier dispositivo móvil.</p>
+	      </div>
+	      <div class="R3">
+	        <embed src="resources/svg/3svg.svg" type="">
+	        <p>Conéctate a nuestro sistema desde cualquiera de tus instrumentos de trabajo y empieza a disfrutar de los beneficios que te ofrece PagoNet.</p>
+	      </div>
+	    </section>
+	  </article>
+	  <article>
+	    <section class="boxPass2">
+	      <div>
+	        <embed src="resources/svg/icon2Pass1.svg" type="">
+	        <p>Indicar los datos del servicio que deseas recargar.</p>
+	      </div>
+	      <div>
+	        <embed src="resources/svg/icon2Pass2.svg" type="">
+	        <p>Cancela el monto de la recarga.</p>
+	      </div>
+	      <div>
+	        <embed src="resources/svg/icon2Pass3.svg" type="">
+	        <p>Recibe notificación y verifica tu saldo.</p>
+	      </div>
+	    </section>
+	  </article>
+	</main>
+	<footer id="informacion">
+	  <section>
+	    <div class="div1foo">
+	      <embed src="resources/svg/logo1.svg" type="">
+	      <p>Apoyamos el crecimiento de las pequeñas empresas y nos encargamos de ofrecer soluciones de pago a los grandes empresarios, adaptándonos a las necesidades de nuestros clientes siendo el mejor socio para sus ventas.</p>
+	      <embed src="resources/svg/RRSS.svg" type=""> 
+	    </div>
+	    <div class="div2foo">
+	      <div>Dirección</div>
+	      <p>Ideal para quienes hasta hoy no podían vender con tarjeta. Profesionales independientes, profesores particulares, emprendedores, instructores, servicio de reparaciones a domicilio, deliveries, ferias, taxis, remises, entre otros.</p>
+	      <embed class="designeBy" src="resources/svg/designedBy.svg" type=""> 
+	    </div>
+	  </section>
+	</footer>
+	</body>
+	<script>
+
+
+	/* for(var x=0; x < document.querySelectorAll('a').length; x++){
+	    document.querySelectorAll('a')[x].classList.remove('active')
+	    document.querySelectorAll('a')[x].onclick = (e) => { 
+	        e.target.classList.add('active')
+	    }
+	} */
+
+	    document.querySelector('.menu').onclick = () => { 
+	        document.querySelector('.ul').classList.toggle('display');
+	        document.querySelector('header').appendChild(document.querySelector('.ul'))
+	    }
+
+	    function myFunction(x) {
+	  if (x.matches) { // If media query matches
+	    document.querySelector('.header').appendChild(document.querySelector('.ul'))
+	    document.querySelector('.ul').classList.add('display');
+	  } 
+	//   else {
+	//    document.body.style.backgroundColor = "pink";
+	//   }
+	}
+
+	var x = window.matchMedia("(min-width: 768px)")
+	myFunction(x) // Call listener function at run time
+	x.addListener(myFunction) // Attach listener function on state changes
+
+	</script>
+	</html>	
+
+
+ ?>
